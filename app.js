db141b375f1ce891363ad0c6a5ebc691db2f64f8
nicostrinna <nicostrinna@hotmail.com>
2021-10-03 15:25:56 -0300

    primer comit

---
+++ app.js
@@ -0,0 +1,17 @@
+let nombreIngresado = prompt("Ingrese su nombre");
+console.log("Nombre:" + nombreIngresado);
+let apellidoIngresado = prompt("Ingrese su apellido");
+console.log("Apellido:" + apellidoIngresado);
+let edadIngresado = prompt("Ingrese su edad");
+console.log("Edad:" + edadIngresado + "años");
+
+alert("Hola" +""+ (nombreIngresado) +""+ "hagamos una cuenta");
+
+let numeroUno = prompt("Ingresa el primero número");
+let numeroDos = prompt("Ingresa el segundo número");
+
+console.log(typeof numeroUno);
+
+let resultado = parseInt(numeroUno) + parseInt(numeroDos);
+console.log(resultado);
+alert(resultado);
